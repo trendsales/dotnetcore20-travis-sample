0c1e59e94ab88d60b97e77a74425363c06d1f185
Kasper Eklander <kaper@eklander.dk>
2017-10-11 13:05:27 +0200

    Test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcore20_travis_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
